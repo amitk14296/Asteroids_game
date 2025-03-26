0b9868c06227e7eff047975eb649cdf660e48463
Amit <amitk14296@gmail.com>
2025-03-26 21:08:43 +0530

    Adding delta time

--- main.py
+++ main.py
@@ -4,17 +4,21 @@
 import pygame
 from constants import*
 def main():
-    pygame.init
+    pygame.init()#initializing the pygame library
     pygame.display.set_mode()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    #print("Starting Asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))#this is taking screen_width and screen_height
+    dt = 0
+    clock = pygame.time.Clock()#creating a pygame.time.Clock() object
+    
+#Starting the game loop
     i = 0
-    while i == 0:
-        for event in pygame.event.get():
+    while i == 0:#loop will run till i is 0(no increment is setup for i to make it a infinite loop)
+        for event in pygame.event.get():#to know when user quit the game(more to be read)
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        pygame.display.flip()
+        screen.fill("black")#here screen is object to the pygame.display.set_mode()
+                            # and we are using fill method to put black color on the screen
+        pygame.display.flip()#update full display surface to the screen
+        dt = (clock.tick(60))/1000
 if __name__ == "__main__":
     main()
